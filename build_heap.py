8cf985d27fe90d95806ff5ac7d11cbb4c10db8c5
rudolfssau-classroom <125464967+rudolfssau-classroom@users.noreply.github.com>
2023-03-19 18:44:24 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -3,40 +3,39 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(len(data)):
+        siftdown(i, data, swaps)
     return swaps
-
-
+def siftdown(i, data, swaps):
+    node = i
+    left_child = 2*i+1
+    right_child = 2*i+2
+    if left_child < len(data) and data[left_child] < data[node]:
+        node = left_child
+    if right_child < len(data) and data[right_child] < data[node]:
+        node = right_child
+    if i != node:
+        data[i], data[node] = data[node], data[i]
+        swaps.append(i, node)
+        siftdown(node, data, swaps)
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
+    input_in = input()
+    if input_in == "I":
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif  input_in == "F":
+        filename = input()
+        if "a" not in filename:
+            with open("./tests/" + filename, mode='r') as f:
+                nav = int(f.readline())
+                data = list(map(int, f.readline().split()))
+    else:
+        print("error")
+        return
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
 if __name__ == "__main__":
     main()
